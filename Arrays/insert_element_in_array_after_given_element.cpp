e783059f5e1b1a8a48aeb854753224c4f4ffc718
Celman <connect.celman@gamil.com>
2022-06-20 20:53:05 +0530

    added,insert_element_in_array_after_given_element.cpp

---
+++ Arrays/insert_element_in_array_after_given_element.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#define MAX_SIZE 100
+using namespace std;
+
+void set_data(int B[], int size_a)
+{
+
+    cout << "Input the elements of an array ";
+    for (int i = 0; i < size_a; i++)
+    {
+        cin >> B[i];
+    }
+}
+
+// printing the array
+void show_data(int D[], int size_a)
+{
+
+    cout << "Original array ";
+    for (int i = 0; i < size_a; i++)
+    {
+        cout << D[i] << " ";
+    }
+}
+
+main()
+
+{
+    int ARR[MAX_SIZE];
+    cout << "Input the size of an array ";
+    int size;
+    cin >> size;
+
+    set_data(ARR, size);
+    show_data(ARR, size);
+
+    cout << "Please input the element you want to insert ";
+    int item;
+    cin >> item;
+
+    cout << "Input the position at which you want to insert an element ";
+    int position;
+    cin >> position;
+
+    if (position <= size && position >= 0)
+    {
+        for (int i = size; i > position; i--)
+        {
+            ARR[i] = ARR[i - 1];
+        }
+        ARR[position] = item;
+
+        cout << " Array after inserting " << item;
+        for (int i = 0; i <= size; i++)
+        {
+            cout << ARR[i] << " ";
+        }
+    }
+    else
+        cout << "Invalid position";
+}
